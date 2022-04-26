7ecc9e527b490dd33e6a657be7182d3cf179fd3a
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2022-04-26 16:42:42 +0200

    reset

--- tests/Type/data/data-list-return-types.php
+++ tests/Type/data/data-list-return-types.php
@@ -4,78 +4,78 @@ namespace DataListDynamicMethodReturnTypesNamespace;
 
 use SilverStripe\CMS\Model\SiteTree;
 use Symbiote\SilverstripePHPStan\ClassHelper;
-use function PHPStan\Testing\assertNativeType;
+use function PHPStan\Testing\assertType;
 
 class Foo
 {
     public function doFoo()
     {
         $siteTreeDataList = SiteTree::get();
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->filter(array("ID" => "1"))
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->filterAny(array("ID" => "1"))
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->reverse()
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->where("1 = 1")
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->whereAny("1 = 1")
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->innerJoin("1 = 1", "TableName")
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->sort(array("ID", "Title"))
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->limit(10)
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->exclude(array("ID" => "1"))
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->setDataQueryParam("Versioned.mode", "all_versions")
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->alterDataQuery(function($query){ $query->reverseSort(); })
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->setQueriedColumns(array("ID"))
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->byIDs(array(3,5))
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->addMany(array(1,2))
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->removeMany(array(1,2))
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->removeByFilter('"ID" = 1')
         );
-        assertNativeType(
+        assertType(
             sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
             $siteTreeDataList->removeAll()
         );
