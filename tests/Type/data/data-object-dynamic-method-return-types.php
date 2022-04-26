224e54ef05e1b04bfae7ab302787f3a9341ca0b4
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2022-04-26 15:51:25 +0200

    DataObjectGetStaticReturnTypeExtensionTest

--- tests/Type/data/data-object-dynamic-method-return-types.php
+++ tests/Type/data/data-object-dynamic-method-return-types.php
@@ -3,12 +3,38 @@
 namespace DataObjectDynamicMethodReturnTypesNamespace;
 
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\ORM\DataObject;
+use function PHPStan\Testing\assertType;
 
 class Foo
 {
 	public function doFoo()
 	{
 		$sitetree = new SiteTree();
+        assertType(
+            sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
+            SiteTree::get()
+        );
+        assertType(
+            sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
+            DataObject::get(ClassHelper::SiteTree)
+        );
+        assertType(
+            sprintf('%s<%s>', ClassHelper::DataList, ClassHelper::SiteTree),
+            DataObject::get(SiteTree::class)
+        );
+        assertType(
+            ClassHelper::SiteTree,
+            SiteTre::get_one()
+        );
+        assertType(
+            ClassHelper::SiteTree,
+            DataObject::get_one(ClassHelper::SiteTree)
+        );
+        assertType(
+            sprintf(ClassHelper::SiteTree),
+            DataObject::get_one(SiteTree::class)
+        );
 		die;
 	}
 }
