846a3877aae251e6b92a62f911d80340cff59ea9
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2025-09-18 09:06:49 +1200

    remove: silverstripe/cms from dependencies (#31)
    - Removed as silverstripe/cms is not required by all silverstripe modules. This will make it easier to use this library with other modules

--- stubs/Page.php
+++ stubs/Page.php
@@ -4,5 +4,9 @@ namespace {
 
     use SilverStripe\CMS\Model\SiteTree;
 
+    if (!class_exists(SilverStripe\CMS\Model\SiteTree::class)) {
+        return;
+    }
+
     class Page extends SiteTree {}
 }
--- stubs/PageController.php
+++ stubs/PageController.php
@@ -4,5 +4,9 @@ namespace {
 
     use SilverStripe\CMS\Controllers\ContentController;
 
+    if (!class_exists(SilverStripe\CMS\Controllers\ContentController::class)) {
+        return;
+    }
+
     class PageController extends ContentController {}
 }
